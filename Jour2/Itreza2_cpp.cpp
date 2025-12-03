44d0780dfbcebad7a4c7ce0373fe8462cf75eba2
BOSSY <louis.bossy@etu.univ-tours.fr>
2025-12-03 11:33:36 +0100

    Jour 2 Partie 2 en c++

--- Jour2/Itreza2_cpp.cpp
+++ Jour2/Itreza2_cpp.cpp
@@ -17,7 +17,7 @@ long long part_1(const char* input) {
 
 	while (std::getline(file, range, ',')) {
 		lower_length = range.find('-');
-		higher_length = range.size() - range.find('-') - 1;
+		higher_length = range.size() - lower_length - 1;
 		if (range[range.size() - 1] == '\n' || range[range.size() - 1] == '\0')
 			higher_length--;
 		lower = std::stoll(range.substr(0, lower_length)); // this is Steve : ('-') , find Steve
@@ -39,10 +39,49 @@ long long part_1(const char* input) {
 	return result;
 }
 
+//[ Partie 2 ]//
+
+long long part_2(const char* input) {
+	std::ifstream file(input);
+	if (!file.is_open())
+		return -1;
+
+	std::string range, candidate_string, candidate_pattern;
+	long long lower, higher, candidate;
+	int lower_length, higher_length;
+	std::vector<long long> invalid_ids;
+
+	while (std::getline(file, range, ',')) {
+		lower_length = range.find('-');
+		higher_length = range.size() - lower_length - 1;
+		if (range[range.size() - 1] == '\n' || range[range.size() - 1] == '\0')
+			higher_length--;
+		lower = std::stoll(range.substr(0, lower_length)); // this is Steve : ('-') , find Steve
+		higher = std::stoll(range.substr(range.find('-') + 1, higher_length));
+
+		for (
+			int pattern = 1;
+			pattern <= higher / std::pow(10, std::floor((float)higher_length / 2));
+			pattern++
+			) {
+			candidate_string = candidate_pattern = std::to_string(pattern);
+			while (candidate_string.length() < 14 && (candidate = std::stoll(candidate_string += candidate_pattern)) <= higher) {
+				if (candidate >= lower && std::find(invalid_ids.begin(), invalid_ids.end(), candidate) == invalid_ids.end())
+					invalid_ids.push_back(candidate);
+			}
+		}
+	}
+	long long result = 0;
+	for (long long id : invalid_ids)
+		result += id;
+	return result;
+}
+
 int main() {
 	const char* input = "Texte.txt";
 
 	std::cout << "Partie 1 : " << part_1(input) << std::endl;
+	std::cout << "Partie 2 : " << part_2(input) << std::endl;
 
 	return 0;
 }
